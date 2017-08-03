a10878604d2d6c0b60fd57a3fcc88020d9715ad9
Vitalii Kulyk <vitcool@ukr.net>
2017-08-03 09:44:42 +0300

    CDS_0: added car-model

--- angular-codeschool/src/app/car-component/car-component.component.ts
+++ angular-codeschool/src/app/car-component/car-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarModel } from "../car-model";
 
 @Component({
   selector: 'app-car-component',
@@ -12,13 +13,15 @@ export class CarComponentComponent implements OnInit {
   ngOnInit() {
   }
 
-  cars = [
+  cars: CarModel[] = [
     {
+      "id": 0,
       "vendor":"VW", 
       "model": "Golf V",
       "price": 8000
     },
     {
+      "id": 1,
       "vendor":"Opel", 
       "model": "Astra",
       "price": 7300
---
+++ angular-codeschool/src/app/car-model.ts
@@ -0,0 +1,6 @@
+export class CarModel {
+    id: number;
+    vendor: string;
+    model: string;
+    price: number;
+}
